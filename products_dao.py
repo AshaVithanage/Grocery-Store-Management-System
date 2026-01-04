4d008efe77441f8434da5890e35a1473a9f866ae
Asha8141 <ashavithanage9@gmail.com>
2026-01-04 10:44:45 +0530

    Implement insert_new_product function for products table

--- products_dao.py
+++ products_dao.py
@@ -1,11 +1,54 @@
-import mysql.connector
+from itertools import product
 
-cnx = mysql.connector.connect(user='root', password='Asha@8141',
-                              host='127.0.0.1',
-                              database='grocery_store')
+from sql_connection import get_sql_connection
 
-cursor = cnx.cursor()
+def get_all_products(connection):
 
-cursor.execute(query)
+    cursor = connection.cursor()
 
-cnx.close()
+    query = ("SELECT products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name "
+             "FROM products inner join uom on products.uom_id=uom.uom_id;")
+
+    cursor.execute(query)
+
+    response = []
+    for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
+        response.append(
+            {
+                'product_id': product_id,
+                'name': name,
+                'uom_id': uom_id,
+                'price_per_unit': price_per_unit,
+                'uom_name': uom_name
+            }
+        )
+
+    return response
+
+
+
+def insert_new_product(connection, products):
+    cursor = connection.cursor()
+    query = ("INSERT INTO products" 
+             "(name, uom_id, price_per_unit)"
+             "VALUES (%s, %s, %s)")
+    data=(products['product_name'], products['uom_id'], products['price_per_unit'])
+    cursor.execute(query, data)
+    connection.commit()
+
+    return cursor.lastrowid
+
+
+
+
+if __name__=='__main__':
+    connection = get_sql_connection()
+    print(insert_new_product(connection, {
+        'product_name': 'cabbage',
+        'uom_id': '1',
+        'price_per_unit': '10',
+
+    }))
+
+
+
